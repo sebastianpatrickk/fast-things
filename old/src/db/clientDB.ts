8a4d66718db1ef31574212c720a3753b51a5f694
Turbobot <turbobot@vercel.com>
2025-02-07 11:51:11 +0100

    feat(create-turbo): apply official-starter transform

--- old/src/db/clientDB.ts
+++
@@ -1,7 +0,0 @@
-import { PGlite } from "@electric-sql/pglite";
-import { drizzle } from "drizzle-orm/pglite";
-
-const client = new PGlite("idb://my-pgdata");
-const db = drizzle({ client });
-
-export { db };
