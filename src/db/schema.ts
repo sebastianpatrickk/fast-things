00f090d752ccfb468c3436a77c65efe0523ca369
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2025-02-09 17:27:08 +0100

    update schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,8 @@
 import {
-  ANYONE_CAN,
   type Row,
-  boolean,
   createSchema,
   definePermissions,
+  relationships,
   string,
   table,
 } from "@rocicorp/zero";
@@ -12,29 +11,39 @@ const user = table("user")
   .columns({
     id: string(),
     name: string(),
-    partner: boolean(),
   })
   .primaryKey("id");
 
-export const schema = createSchema(
-  1, // Schema version. See [Schema Migrations](/docs/migrations) for more info.
-  {
-    tables: [user],
-  },
-);
+const task = table("task")
+  .columns({
+    id: string(),
+    name: string(),
+    status: string(),
+    createdById: string(),
+    assignedToId: string(),
+  })
+  .primaryKey("id");
+
+const taskRelationships = relationships(task, ({ one }) => ({
+  createdBy: one({
+    sourceField: ["createdById"],
+    destSchema: user,
+    destField: ["id"],
+  }),
+  assignedTo: one({
+    sourceField: ["assignedToId"],
+    destSchema: user,
+    destField: ["id"],
+  }),
+}));
+
+export const schema = createSchema(1, {
+  tables: [user, task],
+  relationships: [taskRelationships],
+});
 
 export type Schema = typeof schema;
 export type User = Row<typeof schema.tables.user>;
+export type Task = Row<typeof schema.tables.task>;
 
-export const permissions = definePermissions(schema, () => {
-  return {
-    user: {
-      row: {
-        select: ANYONE_CAN,
-        insert: ANYONE_CAN,
-        update: ANYONE_CAN,
-        delete: ANYONE_CAN,
-      },
-    },
-  };
-});
+export const permissions = definePermissions(schema, () => ({}));
